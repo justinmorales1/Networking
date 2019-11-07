cd9ce0040441e13506f0dc805618b6693309e445
justinmorales1 <jm.em0210@gmail.com>
2019-11-07 16:14:00 -0600

    Updating client code

--- client.py
+++ client.py
@@ -66,42 +66,44 @@
 import time
 from socket import *
 
-pings = 1
+totalNumberOfPings = 0
 lostPackets = 0
+totalRTTTime = 0
+eachRTTTime = 0
 #Send ping 10 times
-while pings < 10000:
-
+for pings in range(10):
+    totalNumberOfPings = totalNumberOfPings + 1
     #Create a UDP socket
     clientSocket = socket(AF_INET, SOCK_DGRAM)
-
     #Set a timeout value of 1 second
-    clientSocket.settimeout(1)
-
+    clientSocket.settimeout(2.0)
     #Ping to server
-    message = 'Sending data to server'
+    message = 'This is the message'
     #Non Losseless Server
     #serveraddress = ("69.61.103.44", 7851)
     #Lossy Server
     serveraddress = ("69.61.103.44", 8591)
 
-    #Send ping
+    #Start Time
     startTime = time.time()
+    #Send message
     clientSocket.sendto(message.encode('utf_8'), serveraddress)
 
-    #If data is received back from server, print
     try:
         data, server = clientSocket.recvfrom(1024)
         endTime = time.time()
-        totalTime = endTime - startTime
+        eachRTTTime = endTime - startTime
+        totalRTTTime = totalRTTTime + eachRTTTime
         print (data)
-        print (pings)
-        print (totalTime)
+        print ("Each rtt time", eachRTTTime)
+        print("The total time of RTT is ", totalRTTTime)
 
     #If data is not received back from server, print it has timed out
     except timeout:
         lostPackets = lostPackets + 1
         print ('REQUEST TIMED OUT')
 
-    pings = pings + 1
-
+print("The total number of packets sent is ", totalNumberOfPings)
+print("The total time of RTT is ", totalRTTTime)
+print("The packet average RTT is ", totalRTTTime/totalNumberOfPings)
 print("The total lost packets are " , lostPackets)

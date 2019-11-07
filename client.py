aba03bdd2df423129c8e173e00846c98945fefe2
justinmorales1 <jm.em0210@gmail.com>
2019-11-07 16:22:39 -0600

    Updating client code

--- client.py
+++ client.py
@@ -1,68 +1,3 @@
-# from socket import *
-# import time
-# import threading
-#
-# PORT_NUMBER = 8581
-# # HOST_NAME = 'localhost'
-# RTT = []
-# #PORT_NUMBER = 7851
-# HOST_NAME = '69.61.103.44'
-# BUFFER_SIZE = 4096
-# ADDRESS = (HOST_NAME,PORT_NUMBER)
-# UDPPacketNumber = 1;
-# TotalRoundTripArray = []
-# count = 0
-# totalRTTTime = 0
-# numberOfLostPackets = 0;
-#
-#
-# #Setup my socket
-# udpSocket = socket(AF_INET,SOCK_DGRAM)
-#
-# msgToSendToClient = "This is for the UDP Server"
-#
-# while count < 1:
-#     #Start time for the RTT
-#     startTime = time.time()
-#
-#     #Send the data to server
-#     udpSocket.sendto(msgToSendToClient.encode('utf_8'),ADDRESS)
-#
-#     print("----- Waiting for response -----")
-#     #The returned data from the server
-#     recvData = udpSocket.recvfrom(BUFFER_SIZE)
-#
-#     #The RTT end time
-#     endTime = time.time()
-#
-#     #The RTT total time
-#     totalTime = str(endTime - startTime)
-#     #Get a total RTT for all the messages sent
-#     totalRTTTime += endTime - startTime
-#
-#     TotalRoundTripArray.append(totalTime)
-#
-#     #Print the total time
-#     print("The Round Trip Time is " + totalTime)
-#
-#     #Print the returned data.
-#     if recvData is not None:
-#        print("The returned data is ----", recvData)
-#     #Count for the number of messages sent
-#     count += 1
-#
-#
-# # numberOfLostPackets = str((10-len(TotalRoundTripArray))*10)
-# print("The total number of packets sent is ", count)
-# print("The number of packets that came back ", len(TotalRoundTripArray))
-# print("The number of packets that were lost", count - len(TotalRoundTripArray))
-# print("The total time of RTT is ", totalRTTTime)
-# print("The packet average RTT is ", totalRTTTime/count)
-#
-# print(TotalRoundTripArray)
-# #Close socket
-# udpSocket.close()
-
 import time
 from socket import *
 
@@ -71,7 +6,7 @@ lostPackets = 0
 totalRTTTime = 0
 eachRTTTime = 0
 #Send ping 10 times
-for pings in range(10):
+for pings in range(100):
     totalNumberOfPings = totalNumberOfPings + 1
     #Create a UDP socket
     clientSocket = socket(AF_INET, SOCK_DGRAM)
@@ -94,9 +29,9 @@ for pings in range(10):
         endTime = time.time()
         eachRTTTime = endTime - startTime
         totalRTTTime = totalRTTTime + eachRTTTime
-        print (data)
-        print ("Each rtt time", eachRTTTime)
-        print("The total time of RTT is ", totalRTTTime)
+        #print (data)
+        #print ("Each rtt time", eachRTTTime)
+        #print("The total time of RTT is ", totalRTTTime)
 
     #If data is not received back from server, print it has timed out
     except timeout:
